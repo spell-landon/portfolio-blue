80017c40d0e5a1d22def12dde45ef1bd8d828ed5
spell-landon <spell.landon@gmail.com>
2022-01-30 12:44:08 -0600

    Change file path

--- src/projects.js
+++ src/projects.js
@@ -6,9 +6,9 @@ export const data = [
     content:
       'Recipeace is a single page application that lets hungry users find recipes on anything they are looking for. I utilized the Edamam API to procure data based on a search query or by clicking the randomize button which will show random recipes.',
     images: {
-      first: require('https://github.com/spell-landon/portfolio-blue/blob/4550aefd2fbfbc8a403b2d969bf58c9b57177301/src/assets/Recipeace/Main%20Landing%20-%20Desktop.png'),
-      second: require('https://github.com/spell-landon/portfolio-blue/blob/4550aefd2fbfbc8a403b2d969bf58c9b57177301/src/assets/Recipeace/Main%20Landing%20-%20Mobile.png'),
-      third: require('https://github.com/spell-landon/portfolio-blue/blob/4550aefd2fbfbc8a403b2d969bf58c9b57177301/src/assets/Recipeace/Recipes%20-%20Mobile.png'),
+      first: 'src/assets/Recipeace/Main Landing - Desktop.png',
+      second: 'src/assets/Recipeace/Main Landing - Mobile.png',
+      third: 'src/assets/Recipeace/Recipes - Mobile.png',
     },
   },
   {
@@ -16,7 +16,7 @@ export const data = [
     github: 'https://github.com/spell-landon/flash-cards',
     link: 'https://spell-landon.github.io/flash-cards/',
     content:
-      "Flash Cards is a browser-based game that tests your knowledge of a given subject. You will have a card that displays a question with answers listed below. You have a scoreboard that keeps track of your current score and will display a 'winning' or 'losing' message atthe end of the game. Also, there's a button on the card which will allow you to 'peek' at the answer.",
+      "Flash Cards is a browser-based game that tests your knowledge of a given subject. You will have a card that displays a question with answers listed below. You have a scoreboard that keeps track of your current score and will display a 'winning' or 'losing' message at the end of the game. Also, there's a button on the card which will allow you to 'peek' at the answer.",
     images: {
       first: 'src/assets/FlashCards/Landing.png',
       second: 'src/assets/FlashCards/Gameplay - Light.png',
