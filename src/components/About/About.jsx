5f58d14bb2b6b7b77c1198958c4df40df4402b27
spell-landon <spell.landon@gmail.com>
2022-02-03 17:38:56 -0600

    Add favicon and change text in About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,20 +15,26 @@ function About(props) {
           id='image'
           href='https://www.linkedin.com/in/landon-spell/'
           target='_blank'
-          rel='noopener noreferrer'>
-        </a>
+          rel='noopener noreferrer'></a>
         <p>
-          Hello, I am Landon Spell. Yeah. George. Y'know this time it wasn't my
-          fault. The Doc set all of his clocks twenty-five minutes slow. Yeah, I
-          think it's a major embarrassment having an uncle in prison. Shut your
-          filthy mouth, I'm not that kind of girl. Whoa, they really cleaned
-          this place up, looks brand new. My god, it's my mother. Put your pants
-          back on. Wow, ah Red, you look great. Everything looks great. 1:24, I
-          still got time. Oh my god. No, no not again, c'mon, c'mon. Hey.
-          Libyans. Doc. Hi, it's really a pleasure to meet you. Here you go,
-          lady. There's a quarter. That was so stupid, Grandpa hit him with the
-          car. Oh, hi , Marty. I didn't hear you come in. Fascinating device,
-          this video unit. A colored mayor, that'll be the day. Mom, Dad.
+          Hello, my name is Landon and I am a full stack software engineer. I am
+          a recent graduate of the Software Engineering Program at General
+          Assembly where I focused on different languages and frameworks. Prior
+          to the program, I was an Estimator and Project Manager for Rainbow
+          International, which taught me not only how to present myself
+          professionally, communicate and articulate difficult concepts, being
+          able to work with a team, and adapt to new methods and
+          implementations, but also how to think outside the box when a
+          difficult problem arose. I now strive to apply that same level of
+          optimism, gumption and dedication towards a Software Development role.
+          My motto is “We'll figure it out”, because if there's a problem that
+          needs solving, an impossible deadline, or something new that needs to
+          be implemented, we can, and will, figure it out and get the job done!
+        </p>
+        <p>
+          Languages & Frameworks: HTML | CSS3 | JavaScript | React | Node |
+          Express | AJAX | Python Management & Deployment: Git/Github | Terminal
+          | Heroku Databases: SQL | PostgreSQL | MongoDB | API's
         </p>
       </div>
     </div>
